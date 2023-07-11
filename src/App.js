be8b138aad7f5c413a802640d238338410e50940
bhagwat <bhagwatt.03@gmail.com>
2023-07-11 23:37:21 +0530

    first program

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button type="button" class="btn btn-primary">Base class</button>
+    </div>
+  );
+}
+
+export default App;
